913690dc64767d712c89538516e4b4b190702552
abhilathiya4 <123169803+abhilathiya4@users.noreply.github.com>
2023-03-23 15:19:51 +0530

    Add files via upload

---
+++ 1house.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+class a{
+	public:
+		
+	int br;
+	int kt;	
+	int bt;
+	int fl;
+	int bl;
+	
+};
+int main(){
+	
+	a d1;
+	
+	cout<<"Enter number bedroom : "; 
+	cin>>d1.br;
+	
+	cout<<"Enter number of kitchen : "; 
+	cin>>d1.kt;
+	
+	cout<<"Enter number of bathroom : "; 
+	cin>>d1.bt;
+	
+	cout<<"Enter number of floor : "; 
+	cin>>d1.fl;
+	
+	cout<<"Enter number of balcony : "; 
+	cin>>d1.bl;
+	
+	cout<<endl;
+	
+	
+	
+
+
+	cout<<d1.br<<endl;
+	cout<<d1.kt<<endl;
+	cout<<d1.bt<<endl;
+	cout<<d1.fl<<endl;
+	cout<<d1.bl<<endl;
+		
+		
+	return 0;
+}
